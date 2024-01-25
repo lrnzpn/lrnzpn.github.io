3a751e5275bf7a4aacb89ed3ebcbb5841a37a8c4
Miggy Pinaroc <lorenzo.pinaroc@paymaya.com>
2024-01-25 23:28:32 +0800

    install dependencies and edit root files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    colors: {
+      black: "var(--main-black)",
+      brown: "var(--main-brown)",
+      "dark-green": "var(--main-dark-green)",
+      gray: "var(--main-gray)",
+      "olive-green": "var(--main-olive-green)",
+      white: "var(--main-white)",
+    },
+    fontFamily: {
+      primary: "var(--primary-font)",
+      secondary: "var(--secondary-font)",
+    },
   },
   plugins: [],
 };
