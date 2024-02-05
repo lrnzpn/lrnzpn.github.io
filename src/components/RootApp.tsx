685cffd2721889fc66d964efa9152196f3789bee
Miggy Pinaroc <lorenzo.pinaroc@paymaya.com>
2024-02-05 16:20:00 +0800

    added meta tags

--- src/components/RootApp.tsx
+++ src/components/RootApp.tsx
@@ -1,3 +1,4 @@
+import MetaTags from "react-meta-tags";
 import { RESUME_DATA } from "src/data/resumeData";
 import About from "./sections/About";
 import Education from "./sections/Education";
@@ -12,6 +13,12 @@ const RootApp = () => {
       className="container relative mx-auto scroll-my-12 overflow-auto p-4 bg-white md:p-16"
       data-testid="rootMain"
     >
+      <MetaTags>
+        <title>Lorenzo Migel Pinaroc</title>
+        <meta name="description" content="My Portfolio Site" />
+        <meta property="og:title" content="LMP" />
+        <meta property="og:image" content="src/assets/images/me.png" />
+      </MetaTags>
       <div className="mx-auto w-full max-w-2xl space-y-8" data-testid="rootSectionWrapper">
         <Info information={RESUME_DATA} />
         <About summary={RESUME_DATA.summary} />
